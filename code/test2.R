cded5275a1145bb194226c0c84324c1debd373f5
Dean Evans <73301622+deanrobertevans@users.noreply.github.com>
2023-09-12 17:40:18 -0400

    Create test2.R

---
+++ code/test2.R
@@ -0,0 +1,13 @@
+require(mailR)
+
+
+
+send.mail(from = "pipeline@deanrobertevans.ca",
+          to = c("devans@birdscanada.org"),
+          subject = "KBA Canada Pipeline",
+          body = "This is a test",
+          smtp = list(host.name = "live.smtp.mailtrap.io", port = 587,
+                      user.name = "api",
+                      passwd = Sys.getenv("MAILTRAP_PASS"), ssl = TRUE),
+          authenticate = TRUE,
+          send = TRUE)
